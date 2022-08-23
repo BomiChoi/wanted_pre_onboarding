86a677eb492e04714fd72c7ea1560afed2bc2ec5
Bomi Choi <hibomi97@gmail.com>
2022-08-24 02:08:41 +0900

    Feat: 채용공고 검색기능 추가
    - 검색에 포함되는 필드: 회사명, 국가, 지역, 채용포지션, 채용내용, 사용기술

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,12 +1,28 @@
+from django.db.models import Q
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
 from .models import Post
 from .serializers import PostSerializer
 
 
 # Create your views here.
 class PostListCreateView(ListCreateAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        search = self.request.GET.get('search', '')
+
+        queryset = Post.objects.all()
+        if search:
+            queryset = queryset.filter(
+                Q(company__name__icontains=search) |
+                Q(company__country__name__icontains=search) |
+                Q(company__region__icontains=search) |
+                Q(position__icontains=search) |
+                Q(content__icontains=search) |
+                Q(skill__icontains=search)
+            )
+        return queryset
 
 
 class PostRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
